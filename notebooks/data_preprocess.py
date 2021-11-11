381481ffb64522012d1c7a1a0bbb5e3bf463c955
Sudhakar M K <sudhakar.mkulkarni@gmail.com>
2021-11-11 13:35:41 +0530

    Add dummy implementations notebooks
    This commit adds dummy functions and notebook for structuring project
    implementation
    
    Signed-off-by: Sudhakar M K <sudhakar.mkulkarni@gmail.com>

---
+++ notebooks/data_preprocess.py
@@ -0,0 +1,7 @@
+def preprocess_csv(nyc_parking_violation_csv):
+    # Load data from CSV
+    # Split and select park of data
+    # Sanitize PySpark dataframe
+    # like remove null values, format columns according to required data type
+    
+    return dataframe_year
