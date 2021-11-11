8a3b73f25b9eda073d1c8298a3040e275c420e59
Sudhakar M K <sudhakar.mkulkarni@gmail.com>
2021-11-11 13:35:41 +0530

    Add dummy implementations notebooks
    This commit adds dummy functions and notebook for structuring project
    implementation
    
    Signed-off-by: Sudhakar M K <sudhakar.mkulkarni@gmail.com>

---
+++ notebooks/basic_analysis.py
@@ -0,0 +1,11 @@
+def violation_frequencey(data_frame):
+    violation_count = 0
+    return violation_count
+
+def violations_by_bodytype(data_frame):
+    body_type = 0
+    return body_type
+
+def violations_by_make(data_frame):
+    make_type = 0
+    return make_type
