b6869ab0afe122d96c07c8c1b1e1ad072e40555d
Sudhakar M K <sudhakar.mkulkarni@gmail.com>
2021-12-09 17:12:13 +0530

    Add season based analysis implementation
    This commit adds season based commit analysis
    Also adds plot for precinct analysis
    
    Signed-off-by: Sudhakar M K <sudhakar.mkulkarni@gmail.com>

--- notebooks/precinct_based_analysis.py
+++ notebooks/precinct_based_analysis.py
@@ -1,4 +1,6 @@
 from pyspark.sql.functions import col
+import matplotlib.pyplot as plt
+
 def violating_precicts(nyc_data, print_enable = False):
     nyc_precints = nyc_data.select('violation_precinct')\
                            .groupBy('violation_precinct')\
@@ -28,8 +30,16 @@ def violation_code_frequency_top3_precincts(nyc_data, print_enable = False):
     filtered_data = nyc_data.filter((col('issuer_precinct') == top3[0]) | (col('issuer_precinct') == top3[1]) | (col('issuer_precinct') == top3[2]))
     violation_frequencies = filtered_data.select('violation_code')\
                                          .groupBy('violation_code')\
-                                         .agg({'violation_code':'count'})
+                                         .agg({'violation_code':'count'})\
+                                         .collect()
     if print_enable:
-        violation_frequencies.show()
+        violations = [row['violation_code'] for row in violation_frequencies]
+        frequencies = [row['count(violation_code)'] for row in violation_frequencies]
 
-    return violation_frequencies.collect()
+        fig, ax = plt.subplots(1, 1, figsize=(20,10))
+        ax.set_title("Violations Vs Frequencies")
+        ax.set_xlabel("Violations")
+        ax.set_ylabel("Frequencies")
+        ax.bar(violations, frequencies)
+
+    return violation_frequencies
