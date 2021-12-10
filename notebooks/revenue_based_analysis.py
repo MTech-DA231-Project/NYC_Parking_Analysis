a573cda4ce71101db157af2eec2519cd2319513a
Sudhakar M K <sudhakar.mkulkarni@gmail.com>
2021-12-10 15:19:11 +0530

    Add graph plot for revenue calculations
    Add graph plot for revenue calculations
    
    Signed-off-by: Sudhakar M K <sudhakar.mkulkarni@gmail.com>

--- notebooks/revenue_based_analysis.py
+++ notebooks/revenue_based_analysis.py
@@ -1,15 +1,42 @@
 from pyspark.sql.functions import count, desc
 import pandas as pd
+import matplotlib.pyplot as plt
 
-def yearly_revenue(data_frame):
-    years = ["2017", "2018", "2019", "2020", "2021", "2022"]
+def yearly_revenue(data_frame, enable_plot=True):
+    years = ["2017", "2018", "2019", "2020", "2021"]
+    revenue = []
+    
     for year in years:
         violation_count = get_violation_df__yearly(data_frame, year)
-        print(year, " : ", accumulatedTax_per_Violation(violation_count)['cost'].sum())
+        revenue.append(accumulatedTax_per_Violation(violation_count)['cost'].sum())
 
-def highest_revenue(data_frame):
+    if enable_plot:
+        fig, ax = plt.subplots(1, 1, figsize=(5,5))
+        ax.set_title("Year Vs Revenue in $")
+        ax.set_xlabel("Year")
+        ax.set_ylabel("Revenue in $")
+        ax.bar(years, revenue)
+
+        fig.savefig('../output/yearly_revenue.png')
+    
+    year_revenue = pd.DataFrame(revenue, columns = ['Revenue in $'], index=years)
+    year_revenue.index.name = 'Year'
+
+    return year_revenue
+
+def highest_revenue(data_frame, enable_plot=True):
     violation_count =  get_violation_df__yearly(data_frame, "")
-    return accumulatedTax_per_Violation(violation_count).head(5)
+    top10_revenues = accumulatedTax_per_Violation(violation_count).head(10)
+    
+    if enable_plot:
+      ax = top10_revenues.plot.bar(x='violation_code', y='cost', figsize=(10, 5))
+      ax.set_title("Violation code vs Revenue")
+      ax.set_xlabel("Violation code")
+      ax.set_ylabel("Revenue")
+      fig = ax.get_figure()
+      fig.savefig('../output/highest_revenue.png')
+
+    return top10_revenues
 
 def calulateTax(violation_code, frequency, dict_map):
     price_rate = dict_map.get(int(violation_code))
@@ -34,6 +61,5 @@ def get_violation_df__yearly(df, year):
     return df.select('violation_code', 'issue_date')\
         .filter(df['issue_date'].rlike(year + "-*"))\
         .groupBy('violation_code')\
-        .agg(count('violation_code')
-             .alias('no_of_tickets'))\
+        .agg(count('violation_code').alias('no_of_tickets'))\
         .sort(desc('no_of_tickets'))
